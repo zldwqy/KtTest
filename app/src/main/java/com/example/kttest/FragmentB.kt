48d4da6661f627dbb61eb0d1e49cfd9b82d6eeab
zhengshipu <zhengshipu@126.com>
2019-11-14 17:08:00 +0800

    优化代码

--- app/src/main/java/com/example/kttest/FragmentB.kt
+++ app/src/main/java/com/example/kttest/FragmentB.kt
@@ -5,11 +5,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.ComponentActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.ViewModelStore
+import com.example.kttest.CommUtils.ifNotNull
 import com.example.kttest.databinding.FragmentBBinding
 import kotlinx.android.synthetic.main.item_cart.*
 
@@ -26,34 +29,25 @@ class FragmentB :Fragment(){
         return bBinding.root
     }
 
-    private fun subscribe(bBinding: FragmentBBinding?) {
+    private fun subscribe(bBinding: FragmentBBinding) {
         val pos = arguments?.getInt("position")
         Log.d(TAG,"position: " + pos)
 
-        pos?.let {
-            activity?.let {
-                //使用ViewModelProvider 替换 ViewModelProviders
-                val viewModel = ViewModelProvider(
-                    it.viewModelStore,
-                    ProductViewModel.ProdecViewmodelFactory()
-                ).get(ProductViewModel::class.java)
 
-                Log.d(TAG, "ProductViewModel: " + viewModel)
-                val product = viewModel?.mProducts?.value?.get(pos)
-                product?.let {
-                    bBinding?.txt?.text = product.toString()
-                }
+        ifNotNull(pos,activity){ pos ,activity ->
 
+            val viewModel = ViewModelProvider(
+                activity.viewModelStore,
+                ProductViewModel.ProdecViewmodelFactory()
+            ).get(ProductViewModel::class.java)
+
+            val product = viewModel.mProducts.value?.get(pos)
+            product?.let {
+                bBinding.txt.text = toString()
             }
         }
 
 
     }
 
-
-    fun <T1, T2> ifNotNull(value1: T1?, value2: T2?, bothNotNull: (T1, T2) -> (Unit)) {
-        if (value1 != null && value2 != null) {
-            bothNotNull(value1, value2)
-        }
-    }
 }
